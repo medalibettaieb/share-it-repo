bd441800d376d9224bbc66fb0df937c2574977ea
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-04-15 09:56:54 +0100

    web project
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

--- share-it/src/main/java/services/interfaces/InformationServicesLocal.java
+++ share-it/src/main/java/services/interfaces/InformationServicesLocal.java
@@ -1,8 +1,20 @@
 package services.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import domain.Information;
 
 @Local
 public interface InformationServicesLocal {
+	Boolean postInfo(Integer idOwner, Information information,
+			Integer idCategory);
 
+	List<Information> findAllPostsByIdCustomer(Integer idCustomer);
+
+	List<Information> findAllPostsByIdFollowedCustomer(
+			Integer idFollowedCustomer, Integer idFollowerCustomer);
+
+	List<Information> findAllFollowedPosts();
 }
--- share-it/src/main/java/services/interfaces/UserManagementLocal.java
+++ share-it/src/main/java/services/interfaces/UserManagementLocal.java
@@ -1,8 +1,24 @@
 package services.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import domain.User;
 
 @Local
 public interface UserManagementLocal {
+	Boolean addUser(User user);
 
+	Boolean deleteUser(Integer idUser);
+
+	Boolean updateUser(User user);
+
+	User findUserById(Integer idUser);
+
+	Boolean deleteDetachedUser(User userDetached);
+
+	List<User> findAllUsers();
+
+	Boolean follow(Integer idFollowed, Integer idFollower);
 }
